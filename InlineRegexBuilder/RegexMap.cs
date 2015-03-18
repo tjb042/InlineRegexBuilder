589f457c88039e41305af58e067234623f8b6585
T. James <tjames042@hotmail.com>
2015-03-18 11:15:41 -0400

    Add a basic Regex dictionary wrapper

---
+++ InlineRegexBuilder/RegexMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IRE {
+
+    public class RegexMap<TKey> : Dictionary<TKey, Regex> {
+
+        public RegexMap() : base() { }
+
+        public RegexMap(int capacity) : base(capacity) { }
+
+        public void Add(TKey key, RegexBuilder builder) {
+            this.Add(key, builder.CreateRegex());
+        }
+
+        public void Add(TKey key, RegexBuilder builder, RegexOptions options) {
+            this.Add(key, builder.CreateRegex(options));
+        }
+
+    }
+
+}
