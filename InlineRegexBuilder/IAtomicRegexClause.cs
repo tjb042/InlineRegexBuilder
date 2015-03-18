58d2e92ddd48d451bf076e5174bfc8b45b42db21
T. James <tjames042@hotmail.com>
2015-03-18 15:39:04 -0400

    Add temporary regex interfaces

---
+++ InlineRegexBuilder/IAtomicRegexClause.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IRE {
+
+    public interface IAtomicRegexClause {
+
+        IAtomicRegexClause BeginString();
+
+        IAtomicRegexClause EndString();
+
+        IAtomicRegexClause AppendText(string input, bool encode);
+
+        IAtomicRegexClause AppendText(char input, bool encode);
+
+        IAtomicRegexClause Or();
+
+
+
+    }
+
+    public interface IModifyRegexClause {
+
+        IAtomicRegexClause Quantifier(int quantity);
+
+    }
+
+    public interface IRegexClause : IAtomicRegexClause, IModifyRegexClause { }
+
+}
